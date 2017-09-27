fcb548d5ac08c9b0d69332c7eca09442ca6f7e2f
yayayay <khangds218@gmail.com>
2017-09-27 14:53:06 +0700

    X

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,8 +21,8 @@ YQuiz.config(function ($stateProvider, $urlRouterProvider, $locationProvider, ez
         });
     $locationProvider.html5Mode(true).hashPrefix('');
     ngMetaProvider.setDefaultTitle('The Best Funny Quiz');
-    ezfbProvider.setInitParams({appId: '1706155966071399', version: 'v2.8'});
-    ezfbProvider.setLocale('en_EN');
+    // ezfbProvider.setInitParams({appId: '1706155966071399', version: 'v2.8'});
+    // ezfbProvider.setLocale('en_EN');
 });
 
 
