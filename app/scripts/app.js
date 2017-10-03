92bcb8cf09f6cd56dee8380ece414bec871347c4
yayayay <khangds218@gmail.com>
2017-10-03 17:03:38 +0700

    X

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,7 +20,7 @@ YQuiz.config(function ($stateProvider, $urlRouterProvider, $locationProvider, ez
             }
         });
     $locationProvider.html5Mode(true).hashPrefix('');
-    ngMetaProvider.setDefaultTitle('The Best Funny Quiz');
+    ngMetaProvider.setDefaultTitle('Yquizz.com - The funniest quizzes');
     // ezfbProvider.setInitParams({appId: '1706155966071399', version: 'v2.8'});
     // ezfbProvider.setLocale('en_US');
 });
