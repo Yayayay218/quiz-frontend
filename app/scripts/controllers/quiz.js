4a2f8264a74d85caa9dc669c19f5dd2e2f390d1a
yayayay <khangds218@gmail.com>
2017-10-03 15:05:23 +0700

    X

--- app/scripts/controllers/quiz.js
+++ app/scripts/controllers/quiz.js
@@ -84,7 +84,6 @@ angular.module('YQuiz')
                                 action_type: 'og.shares',
                                 action_properties: JSON.stringify({
                                     object: {
-                                        'app_id': '1706155966071399',
                                         'og:url': $scope.urlFB,
                                         'og:title': $scope.titleShare,
                                         'og:image': $scope.thumbShare
@@ -113,7 +112,6 @@ angular.module('YQuiz')
                         action_type: 'og.shares',
                         action_properties: JSON.stringify({
                             object: {
-                                'app_id': '1706155966071399',
                                 'og:url': $scope.urlFB,
                                 'og:title': $scope.titleShare,
                                 'og:image': $scope.thumbShare
